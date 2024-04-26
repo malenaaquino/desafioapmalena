a35eb1a4a85581b95fbb137ba1981266cf770190
Malena Aquino <aylen.aquino08@gmail.com>
2024-04-26 16:10:52 -0300

    desafio 1

---
+++ clase02/main.js
@@ -0,0 +1,6 @@
+
+let nombreProducto = ("galletitas")
+let precioProducto = (1500)
+let cantidadDeseada = prompt("ingrese cantidad deseada: ")
+let preciofinal = (precioProducto * cantidadDeseada )
+alert("su precio final es:" + preciofinal)
